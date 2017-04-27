a929f8f2249dea23ce080316a271cfcc96e3e34f
iastanley <iasvks.craigslist@gmail.com>
2017-04-26 17:09:33 -0700

    added starter files

---
+++ src/components/number-input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Update this component
+export default function NumberInput(props) {
+    return (
+        <div className="form-group">
+            <label htmlFor={props.id}>{props.label}</label>
+            <input type="number" id={props.id} min={props.min} max={props.max} />
+        </div>
+    );
+}
