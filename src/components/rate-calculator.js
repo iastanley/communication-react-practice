fd62732de2cdaedcfa3de0144e77d0967ac8579d
iastanley <iasvks.craigslist@gmail.com>
2017-04-26 17:51:28 -0700

    small formatting change

--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -16,7 +16,7 @@ export default class RateCalculator extends React.Component {
                 <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
                 <NumberInput id="hours" label="Hours" min={1} max={12} />
                 <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
+                />
             </form>
         );
     }
