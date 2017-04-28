351d0b69d48f76f9e6bce1a078c70dd2e9655af2
iastanley <iasvks.craigslist@gmail.com>
2017-04-28 15:32:48 -0700

    finished rate calculator exercise

--- src/components/number-input.js
+++ src/components/number-input.js
@@ -5,7 +5,9 @@ export default function NumberInput(props) {
     return (
         <div className="form-group">
             <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
+            <input type="number" id={props.id} min={props.min} max={props.max}
+            value={props.value}
+            onChange={(e)=>props.onChange(e.target.value)}/>
         </div>
     );
 }
