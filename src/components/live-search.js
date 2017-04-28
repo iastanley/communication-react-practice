6a9beb394fad6a22e6511ae98a9fea46fb2e07e3
iastanley <iasvks.craigslist@gmail.com>
2017-04-28 16:47:03 -0700

    refactored the live-search example to work

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -3,11 +3,28 @@ import React from 'react';
 import SearchInput from './search-input';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
+//make life-search a stateful component
+export default class LiveSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    };
+  }
+
+  //render
+  render() {
+    const characters = this.props.characters.filter(character => {
+      return character.name.toLowerCase().includes(
+        this.state.searchTerm.toLowerCase()
+      );
+    });
     return (
         <div className="live-search">
-            <SearchInput />
-            <CharacterList characters={props.characters} />
+            <SearchInput
+            onChange={searchTerm => this.setState({searchTerm})}/>
+            <CharacterList characters={characters} />
         </div>
     );
+  }
 }
--- src/components/search-input.js
+++ src/components/search-input.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function SearchInput(props) {
-    return <input type="search" name="Search" placeholder="Dale Cooper" />;
+    return <input type="search" name="Search" placeholder="Dale Cooper" onChange={(e)=> props.onChange(e.target.value)}/>;
 }
